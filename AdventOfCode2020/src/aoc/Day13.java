d8b9d8866fbfbc7c55e9f804e63cb04560c6086f
Tomislav Prhat <tomislav.prhat@fer.hr>
2020-12-13 11:41:08 +0100

    Day 13 solution

---
+++ AdventOfCode2020/src/aoc/Day13.java
@@ -0,0 +1,49 @@
+package aoc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day13 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<String> s = new ArrayList<>();
+		while (sc.hasNextLine()) {
+			s.add(sc.nextLine());
+		}
+		sc.close();
+		
+		int sum = Integer.parseInt(s.get(0));
+		List<Integer> list = new ArrayList<>();
+		for(String x : s.get(1).split(",")) {
+			if(!x.equals("x")) {
+				list.add(Integer.parseInt(x));
+				
+			}
+		}
+		List<Integer> listmax = new ArrayList<>();
+		listmax.addAll(list);
+		boolean change = true;
+		while(change) {
+			change = false;
+			for(int i = 0 ; i < list.size(); i++) {
+				if(listmax.get(i) < sum) {
+					listmax.set(i, listmax.get(i) + list.get(i));
+					change = true;
+				}
+			}
+		}
+		int min = Collections.min(listmax);
+		int j = 0;
+		for(int i = 0; i < listmax.size(); i++) {
+			if(listmax.get(i) == min)
+				j = list.get(i);
+		}
+		int multi = j * (min - sum);
+		System.out.println("Part 1: \n" + multi);
+		
+	}
+
+}
